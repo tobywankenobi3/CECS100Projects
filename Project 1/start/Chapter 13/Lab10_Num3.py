0358125511ec0b4427c85557c43c33d278d36c3c
abadanfernee <abadanfernee@gmail.com>
2016-11-30 11:38:49 -0800

    nice

--- Project 1/start/Chapter 13/Lab10_Num3.py
+++ Project 1/start/Chapter 13/Lab10_Num3.py
@@ -24,7 +24,7 @@ def select(choice):
         if choice == "decrypt":
 
 
-# Display File Text
+# Display File Text - Anfernee
 def display():
     print("Original Message: "+message)
     if not message == original:
@@ -36,12 +36,61 @@ def display():
 def caesar():
     print(message)
 
-# Claw Creator - Sean
+# Claw Creator - Sean/Anfernee
 def claw():
     for letter in message:
         if letter == 'a':
             letter = "|"
-            newmessage.
+        elif letter == 'b':
+            letter = "|\\"
+        elif letter == 'c':
+            letter = "||"
+        elif letter == 'd':
+            letter = "|//"
+        elif letter == 'e':
+            letter = "\\"
+        elif letter == 'f':
+            letter = "||\\"
+        elif letter == 'g':
+            letter = "|||"
+        elif letter == 'h':
+            letter = "||/"
+        elif letter == 'i':
+            letter = "/"
+        elif letter == 'j':
+            letter = "|\\\\"
+        elif letter == 'k':
+            letter = "|\\|"
+        elif letter == 'l':
+            letter = "|\\/"
+        elif letter == 'm':
+            letter = "|/\\"
+        elif letter == 'n':
+            letter = "|/|"
+        elif letter == 'o':
+            letter = "|//"
+        elif letter == 'p':
+            letter = "\\\\"
+        elif letter == 'q':
+            letter = "\\|"
+        elif letter == 'r':
+            letter = "\\/"
+        elif letter == 's':
+            letter = "/\\"
+        elif letter == 't':
+            letter = "/|"
+        elif letter == 'u':
+            letter = "//"
+        elif letter == 'v':
+            letter = "||\\\\"
+        elif letter == 'w':
+            letter = "||//"
+        elif letter == 'x':
+            letter = "|||\\"
+        elif letter == 'y':
+            letter = "|||/"
+        elif letter == 'z':
+            letter = "||||"
 
 def main():
     print("Shite")
