4e6e05ed4d6309addd770cf3fd8a2c0e7506dc81
Sean Cuurley <seancurley007@gmail.com>
2016-11-30 09:43:14 -0800

    Lab 10 Start - Ciphers

---
+++ Project 1/start/Chapter 13/Lab10_Num3.py
@@ -0,0 +1,23 @@
+"""Cipher Encryptor and Decrypter"""
+
+# Encrypt
+
+# Decrypt
+
+# Cipher Selection
+
+# File Read
+
+# Display File Text
+
+# Enigma Creator
+
+# Caesar Shift Creator
+
+# Claw Creator
+
+def main():
+    print("Shite")
+
+
+main()
